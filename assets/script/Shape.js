539ad41f792fbc7720d1b229f249f0e762199a81
Navjot Kaur <saininavjot357019@gmail.com>
2024-03-22 12:23:59 -0500

    Created class

--- assets/script/Shape.js
+++ assets/script/Shape.js
@@ -1,2 +1,22 @@
 'use strict';
 
+export default 'Shape';
+
+class Shape {
+  constructor(name, colour) {
+    this._name = name;
+    this._colour = colour;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get colour() {
+    return this._colour;
+  }
+
+  getInfo() {
+    return `${this._colour} ${this._name}`;
+  }
+}
--- assets/script/app.js
+++ assets/script/app.js
@@ -1,2 +1,3 @@
 'use strict';
 
+import { Shape } from './Shape.js';
